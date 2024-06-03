718347b5ff02743295a9729283377b47311efbf8
Danil <dbaev00@gmail.com>
2024-06-03 12:04:17 +0300

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView counterTextView;
+    private Button clickButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Находим TextView и Button по их ID
+        TextView counterTextView = findViewById(R.id.counterTextView);
+
+        clickButton = findViewById(R.id.clickButton);
+
+        // Устанавливаем слушатель кликов на кнопку
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // При клике увеличиваем счетчик и обновляем текст в TextView
+                counter++;
+                counterTextView.setText(String.valueOf(counter));
+            }
+        });
+    }
+}
+
+
